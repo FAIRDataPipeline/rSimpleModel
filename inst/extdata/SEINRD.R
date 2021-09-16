41e2818ac1c24b5dc93f8e7fa3910d1539992847
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-09-16 19:58:25 +0100

    put the trailing args back in

--- inst/extdata/SEINRD.R
+++ inst/extdata/SEINRD.R
@@ -4,7 +4,7 @@ library(deSolve)
 library(ggplot2)
 
 # Read config directory from command line
-conf.dir <- Sys.getenv("FDP_CONFIG_DIR")
+conf.dir <- commandArgs(trailingOnly=TRUE)[1]
 
 # Initialise code run
 config <- file.path(conf.dir, "config.yaml")
