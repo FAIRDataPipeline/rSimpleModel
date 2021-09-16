7e2c2d227b37985c95610092c5f5b8d0f4da04fe
Richard Reeve <git@richardreeve.net>
2021-09-16 10:17:58 +0100

    Simplify argument to SEIRS_model() and improve user config

--- inst/extdata/SEIRS.R
+++ inst/extdata/SEIRS.R
@@ -2,35 +2,36 @@ library(rSimpleModel)
 library(rDataPipeline)
 library(ggplot2)
 library(dplyr)
+library(magrittr)
 
 # Initialise code run
-config <- file.path(Sys.getenv("FDP_CONFIG_DIR"), "config.yaml")
-script <- file.path(Sys.getenv("FDP_CONFIG_DIR"), "script.sh")
+conf.dir <- Sys.getenv("FDP_CONFIG_DIR")
+config <- file.path(conf.dir, "config.yaml")
+script <- file.path(conf.dir, "script.sh")
 handle <- initialise(config, script)
 
-# Read code run inputs
-static_params <- read.csv(link_read(handle, "disease/sars_cov2/SEIRS_model/parameters/static_params"))
+# Read model parameters
+params <- read.csv(link_read(handle, "SEIRS_model/parameters"))
+a <- params %>% filter(param == "alpha") %$% value
+b <- params %>% filter(param == "beta") %$% value
+ig <- params %>% filter(param == "inv_gamma") %$% value
+io <- params %>% filter(param == "inv_omega") %$% value
+im <- params %>% filter(param == "inv_mu") %$% value
+is <- params %>% filter(param == "inv_sigma") %$% value
+
+# Set initial state
+initial.state <- data.frame(S = 0.999, E = 0.001, I = 0, R = 0)
 
 # Run the model
-results <- SEIRS_model(S = 0.999,
-                       E = 0.001,
-                       I = 0,
-                       R = 0,
-                       timesteps = 1000,
-                       years = 5,
-                       alpha = filter(static_params, param == "alpha")$value,
-                       beta = filter(static_params, param == "beta")$value,
-                       inv_gamma = filter(static_params, param == "inv_gamma")$value,
-                       inv_omega = filter(static_params, param == "inv_omega")$value,
-                       inv_mu = filter(static_params, param == "inv_mu")$value,
-                       inv_sigma = filter(static_params, param == "inv_sigma")$value)
+results <- SEIRS_model(initial.state, timesteps = 1000, years = 5,
+                       alpha = a, beta = b, inv_gamma = ig,
+                       inv_omega = io, inv_mu = im, inv_sigma = is)
 g <- plot_SEIRS(results)
 
 # Save outputs to data store
-path <- link_write(handle, "disease/sars_cov2/SEIRS_model/results/model_output")
-write.csv(results, path)
+results %>% write.csv(link_write(handle, "SEIRS_model/results/model_output"))
 
-path <- link_write(handle, "disease/sars_cov2/SEIRS_model/results/figure")
+path <- link_write(handle, "SEIRS_model/results/figure")
 ggsave(path, g)
 
 # Register code run in local registry
