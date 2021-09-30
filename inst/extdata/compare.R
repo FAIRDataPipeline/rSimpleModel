aac54bab8072868a99408425b188357befbc89e0
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-09-30 17:12:55 +0100

    update script

--- inst/extdata/compare.R
+++ inst/extdata/compare.R
@@ -12,31 +12,23 @@ handle <- initialise(config, script)
 
 # Read model results
 R <- read.csv(link_read(handle, "SEIRS_model/results/model_output/R"))
-python <- read.csv("/Users/Soniam/.fair/data/ryan/SEIRS_model/results/model_output/python/56d139c3b3d70eb7e25b0d424087e965cc3cdd2a.csv")
-# python <- read.csv(link_read(handle, "SEIRS_model/results/model_output/python"))
-colnames(python)[1] <- "time"
-python$time <- python$time / 365.25
+python <- read.csv(link_read(handle, "SEIRS_model/results/model_output/python"))
 julia <- read.csv(link_read(handle, "SEIRS_model/results/model_output/julia"))
 java <- read.csv(link_read(handle, "SEIRS_model/results/model_output/java"))
-colnames(java) <- colnames(R)
 
 raise_issue(handle = handle,
             data_product = "SEIRS_model/results/model_output/java",
-            version = "0.0.1",
-            namespace = "bram",
             issue = "Model has been run over 3649 timesteps rather than 1000",
             severity = 3)
 raise_issue(handle = handle,
             data_product = "SEIRS_model/results/model_output/java",
-            version = "0.0.1",
-            namespace = "bram",
             issue = "Model has assumed there to be 365 days in a year rather than 365.25",
             severity = 5)
 
 # Largest difference between implementations across timesteps
-results <- max(R - python)
+one <- max(R - python)
 two <- max(R - julia)
-three <- max(R - java)
+results <- max(one, two)
 
 # Plot results
 g <- plot_compare(R, python, julia, java)
