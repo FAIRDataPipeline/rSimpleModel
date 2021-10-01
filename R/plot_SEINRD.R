f589370c033fbd1d580839ffeceafb8a2c01fd53
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-10-01 10:48:44 +0100

    edit plot

--- R/plot_SEINRD.R
+++ R/plot_SEINRD.R
@@ -5,22 +5,29 @@
 #' @export
 #'
 plot_SEINRD <- function(results) {
-  ggplot2::ggplot(as.data.frame(results)) +
-    ggplot2::geom_line(ggplot2::aes(x = time, y = S, color = "darkgreen")) +
-    ggplot2::geom_line(ggplot2::aes(x = time, y = E, color = "yellow")) +
-    ggplot2::geom_line(ggplot2::aes(x = time, y = I, color = "red")) +
-    ggplot2::geom_line(ggplot2::aes(x = time, y = R, color = "blue")) +
-    ggplot2::geom_line(ggplot2::aes(x = time, y = D, color = "black")) +
-    ggplot2::geom_vline(xintercept = 82, linetype = "dashed") +
-    ggplot2::geom_vline(xintercept = 340, linetype = "dashed") +
-    ggplot2::scale_color_identity(breaks = c("darkgreen", "yellow","red","blue","black"),
-                                  labels = c("S","E","I","R","D"), guide = "legend") +
-    ggplot2::labs(color = ggplot2::element_blank()) +
-    ggplot2::xlab("Time") +
-    ggplot2::ylab("Number") +
-    ggplot2::theme(axis.title = ggplot2::element_text(size = 15),
-                   axis.text = ggplot2::element_text(size = 13),
-                   legend.text = ggplot2::element_text(size = 15)) +
-    ggplot2::scale_y_continuous(breaks = c(0, 20000000,40000000,60000000),
-                                limits = c(0, 60000000))
+  cols <- c("black", "#aceb4e", "red", "#28cce8", "purple", "yellow")
+
+  left <- results %>%
+    as.data.frame() %>%
+    tidyr::pivot_longer(cols = c(S, E, I, N, R, D)) %>%
+    dplyr::mutate(name = factor(name,
+                                levels = c("S", "E", "I", "N", "R", "D")),
+                  plot = "left")
+
+  right <- tmp %>%
+    dplyr::filter(name %in% c("E", "I", "N", "D")) %>%
+    dplyr::mutate(plot = "right")
+
+  rbind.data.frame(left, right) %>%
+    ggplot2::ggplot() + ggplot2::theme_bw() +
+    ggplot2::geom_line(ggplot2::aes(x = time, y = value,
+                                    group = name, color = name)) +
+    ggplot2::facet_wrap(~plot, scales = "free") +
+    ggplot2::scale_colour_manual(values = cols) +
+    ggplot2::labs(title = "SEINRD model trajectories",
+                  x = "Time", y = "Number") +
+    ggplot2::scale_y_continuous(labels = function(x) format(x, scientific = TRUE)) +
+    ggplot2::theme(strip.background = element_blank(),
+                   strip.text.x = element_blank(),
+                   plot.title = element_text(hjust = 0.5))
 }
